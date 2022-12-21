6515816e84ada1cf7f96ff7eaf7efe7d96e81bfb
guferrei <guferrei@student.42sp.org.br>
2022-12-21 17:25:03 -0300

    php header file fixed

--- srcs/requirements/wordpress/conf/wp-config.php
+++ srcs/requirements/wordpress/conf/wp-config.php
@@ -1,9 +1,10 @@
-<!-- Getting env variables -->
+<?php
+// Getting env variables
 $DB_NAME = getenv('DB_NAME')
 $DB_USER = getenv('DB_USER')
 $DB_PASSWORD = getenv('DB_PASS')
 
-<!-- Defining WP variables -->
+// Defining WP variables
 define('DB_NAME', $DB_NAME)
 
 define('DB_USER', $DB_USER);
@@ -18,20 +19,19 @@ define( 'DB_COLLATE', '' )
 
 \$table_prefix = 'wp_'
 
-<!-- define( 'WP_DEBUG', false )
-
-if ( ! defined( 'ABSPATH' ) ) {
-define( 'ABSPATH', __DIR__ . '/' )
-}
-define( 'WP_REDIS_HOST', 'redis' )
-
-define( 'WP_REDIS_PORT', 6379 )
-
-define( 'WP_REDIS_TIMEOUT', 1 )
-
-define( 'WP_REDIS_READ_TIMEOUT', 1 )
-
-define( 'WP_REDIS_DATABASE', 0 )
-
-require_once ABSPATH . 'wp-settings.php'
- -->
+// define( 'WP_DEBUG', false )
+// 
+// if ( ! defined( 'ABSPATH' ) ) {
+// define( 'ABSPATH', __DIR__ . '/' )
+// }
+// define( 'WP_REDIS_HOST', 'redis' )
+// 
+// define( 'WP_REDIS_PORT', 6379 )
+// 
+// define( 'WP_REDIS_TIMEOUT', 1 )
+// 
+// define( 'WP_REDIS_READ_TIMEOUT', 1 )
+// 
+// define( 'WP_REDIS_DATABASE', 0 )
+// 
+// require_once ABSPATH . 'wp-settings.php'
