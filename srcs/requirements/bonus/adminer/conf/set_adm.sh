28f06404514092b552825556422af70cd42d5852
guferrei <guferrei@student.42sp.org.br>
2023-01-03 12:34:28 -0300

    adminer dockerfile and config

---
+++ srcs/requirements/bonus/adminer/conf/set_adm.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+sed -i "s|listen = 127.0.0.1:9000|listen = 8000|g" \
+/etc/php8/php-fpm.d/www.conf && \
+sed -i "s|;listen.owner = nobody|listen.owner = nobody|g" \
+/etc/php8/php-fpm.d/www.conf && \
+sed -i "s|;listen.group = nobody|listen.group = nobody|g" \
+/etc/php8/php-fpm.d/www.conf
+
+php-fpm8 --nodaemonize
